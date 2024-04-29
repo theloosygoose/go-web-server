a2d97bce9211ce11c10108b23565b354ce59d84c
theloosygoose <g.ramahenry2439@gmail.com>
2024-04-28 20:32:44 -0400

    photo upload

--- internal/handler/admin.go
+++ internal/handler/admin.go
@@ -2,10 +2,14 @@ package handler
 
 import (
 	"database/sql"
-	"github.com/theloosygoose/goserver/internal/types"
-	"github.com/theloosygoose/goserver/internal/view/admin"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/theloosygoose/goserver/internal/types"
+	"github.com/theloosygoose/goserver/internal/view/admin"
 )
 
 type AdminHandler struct {
@@ -14,16 +18,51 @@ type AdminHandler struct {
 
 func (h AdminHandler) AdminAddPhoto() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		query := `INSERT INTO photos 
-        (name, location, date, imagepath, avaliable)
-        VALUES($1, $2, $3, $4, $5);`
 
 		details := types.Photo{
 			Name:      r.FormValue("name"),
 			Location:  r.FormValue("location"),
 			Date:      r.FormValue("date"),
-			ImagePath: r.FormValue("imagepath"),
 		}
+
+        file, fileHeader, err := r.FormFile("imageFile")
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        defer file.Close()
+        log.Println("---UPLOADING PHOTO---")
+        log.Printf("Name:: %v\n", fileHeader.Filename)
+        log.Printf("Size:: %v\n", fileHeader.Size)
+
+        contentType := fileHeader.Header["Content-Type"][0]
+        log.Printf("Content-Type:: %v\n", contentType)
+
+        var osFile *os.File
+
+        if contentType == "image/jpeg"{
+            osFile, err = os.CreateTemp("/mnt/usb/images/", "*.jpg")
+        } else {
+            osFile, err = os.CreateTemp("/mnt/usb/etc/", "")
+        }
+        log.Println("Error In Content-Type", err)
+        defer osFile.Close()
+
+        // SAVE FILE
+        fileBytes, err := io.ReadAll(file)
+        if err != nil {
+            fmt.Println(err)
+        }
+        osFile.Write(fileBytes)
+        defer osFile.Close()
+
+        details.ImagePath = osFile.Name() 
+
+        log.Println("---FILE UPLOAD COMPLETE---")
+
+		query := `INSERT INTO photos 
+        (name, location, date, imagepath, avaliable)
+        VALUES($1, $2, $3, $4, $5);`
 
 		results, err := h.DB.Exec(query, &details.Name, &details.Location, &details.Date, &details.ImagePath, true)
 		if err != nil {
