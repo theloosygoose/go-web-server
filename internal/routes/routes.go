1e959188cfc52655d4d66c70498f73e2ea3aa2b0
theloosygoose <g.ramahenry2439@gmail.com>
2024-04-30 16:15:12 -0400

    working dist folder in .env

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"database/sql"
 	"net/http"
+	"os"
 
 	"github.com/theloosygoose/goserver/internal/handler"
 )
@@ -12,8 +13,9 @@ func NewServer(db *sql.DB) *http.ServeMux {
 
     pHandler := handler.PhotoHandler{DB: db}
     aHandler := handler.AdminHandler{DB: db}
+    dist := os.Getenv("STATIC_DIR")
 
-    fs := http.FileServer(http.Dir("./internal/view/dist"))
+    fs := http.FileServer(http.Dir(dist))
 
     r.Handle("/dist/", http.StripPrefix("/dist/", fs))
 
