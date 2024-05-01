89b06393940bfe96d85cbcae426ab0dc8b354c05
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-05-01 10:50:24 -0400

    load env in main

--- cmd/goserver/main.go
+++ cmd/goserver/main.go
@@ -4,18 +4,26 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
+
 	"github.com/theloosygoose/goserver/internal/routes"
 	db "github.com/theloosygoose/goserver/internal/tools"
 )
 
 func main() {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Println("Loaded Env File")
+    }
+
     DB := db.Connect()
     db.CreateTable(DB)
 
 	r := routes.NewServer(DB)
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Fatal(err)
+
+	nerr := http.ListenAndServe(":8080", r)
+	if nerr != nil {
+		log.Fatal(nerr)
 	}
 
     db.CloseConnection(DB)
