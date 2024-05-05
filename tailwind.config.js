da35538888aaa56df904da20c76f05950b59e011
theloosygoose <g.ramahenry2439@gmail.com>
2024-05-04 23:22:32 -0400

    htmx added transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,13 @@ module.exports = {
         },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        plugin(function({ addVariant }) {
+          addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
+          addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &'])
+          addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
+          addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &'])
+        }),
+    ],
 }
 
