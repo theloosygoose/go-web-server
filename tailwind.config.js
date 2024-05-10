b10cac86ad5b390e3e783a8b0a109c7346ee8140
theloosygoose <g.ramahenry2439@gmail.com>
2024-05-09 21:57:29 -0400

    fixed the form for validations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,18 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
     content: ["./internal/view/**/*.templ"],
     theme: {
-        colors: {
-            'text': '#090906',
-            'background': '#fdfdfc',
-            'primary': '#998d70',
-            'secondary': '#d3c39c',
-            'accent': '#90c072',
-        },
         fontFamily: {
             mono: ['Inconsolata', 'monospace']
         },
-        extend: {},
+        extend: {
+            colors: {
+                'text': '#090906',
+                'background': '#fdfdfc',
+                'primary': '#998d70',
+                'secondary': '#d3c39c',
+                'accent': '#90c072',
+            },
+        },
     },
     plugins: [
         plugin(function({ addVariant }) {
