57609b8b2b417c781c3f6be6e58b54f3d9f81d56
theloosygoose <g.ramahenry2439@gmail.com>
2024-05-14 18:10:57 -0400

    fixed startup db query

--- internal/tools/db.go
+++ internal/tools/db.go
@@ -32,23 +32,10 @@ func CloseConnection(db *sql.DB) {
 }
 
 func CreateTable(db *sql.DB) {
-	var exists bool
-
-	if err := db.QueryRow("SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'photos' );").Scan(&exists); err != nil {
-		fmt.Println("failed to execute query", err)
-		return
-	}
-
-	if !exists {
-		results, err := db.Query("CREATE TABLE photos (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, location VARCHAR(100) NOT NULL, date VARCHAR(100), imagepath VARCHAR(100) NOT NULL, description TEXT, i_height VARCHAR(10), i_width VARCHAR(10));")
-		if err != nil {
-			fmt.Println("failed to execute query", err)
-			return
-		}
-		fmt.Println("Table created successfully", results)
-
-	} else {
-
-		fmt.Println("Table 'photos' already exists ")
-	}
+    results, err := db.Query("CREATE TABLE IF NOT EXISTS photos (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, location VARCHAR(100) NOT NULL, date VARCHAR(100), imagepath VARCHAR(100) NOT NULL, description TEXT, i_height VARCHAR(10), i_width VARCHAR(10));")
+    if err != nil {
+        fmt.Println("failed to execute query", err)
+        return
+    }
+    fmt.Println("Table created successfully", results)
 }
