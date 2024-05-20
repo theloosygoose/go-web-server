05b9fd08df7238c2857f41861d7cd356c4c3263b
theloosygoose <g.ramahenry2439@gmail.com>
2024-05-20 13:37:31 -0400

    admin changes

--- internal/handler/admin.go
+++ internal/handler/admin.go
@@ -26,12 +26,12 @@ func (h AdminHandler) AdminAddPhoto() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.ParseMultipartForm(20)
 
-        var response types.Response
+		var response types.Response
 
 		details := types.Photo{
-			Name:     r.FormValue("name"),
-			Location: r.FormValue("location"),
-            Description: r.FormValue("description"),
+			Name:        r.FormValue("name"),
+			Location:    r.FormValue("location"),
+			Description: r.FormValue("description"),
 		}
 
 		file, fileHeader, err := r.FormFile("imageFile")
@@ -40,28 +40,28 @@ func (h AdminHandler) AdminAddPhoto() http.HandlerFunc {
 			return
 		}
 
-        ImageProcess(file, fileHeader, &details)
+		ImageProcess(file, fileHeader, &details)
 		log.Println("---FILE UPLOAD COMPLETE---")
 
 		query := `INSERT INTO photos 
         (name, location, date, description, imagepath, i_height, i_width)
         VALUES(?, ?, ?, ?, ?, ?, ?);`
 
-		results, err := h.DB.Exec(query, 
-            &details.Name, &details.Location, &details.Date, &details.Description,
-            &details.Image.FileName, &details.Image.Height, &details.Image.Width)
+		results, err := h.DB.Exec(query,
+			&details.Name, &details.Location, &details.Date, &details.Description,
+			&details.Image.FileName, &details.Image.Height, &details.Image.Width)
 		if err != nil {
 			log.Println("Failed to Exectue Query: ", err)
-            response.Message = "Failed to Execute Query"
-            response.Code = http.StatusInternalServerError
+			response.Message = "Failed to Execute Query"
+			response.Code = http.StatusInternalServerError
 		} else {
-            response.Message = "Successful"
-            response.Code = http.StatusOK
-        }
+			response.Message = "Successful"
+			response.Code = http.StatusOK
+		}
 
 		log.Println(results.RowsAffected())
 
-        render(w, r, components.ReponseShow(response))
+		render(w, r, components.ReponseShow(response))
 	})
 
 }
@@ -74,29 +74,29 @@ func (h AdminHandler) HandlerAdminShow() http.HandlerFunc {
 }
 
 func (h AdminHandler) HandlerAdminDeletePhoto() http.HandlerFunc {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        id := r.PathValue("id") 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
 
-        query := `DELETE FROM photos WHERE id = ? RETURNING imagepath;`
+		query := `DELETE FROM photos WHERE id = ? RETURNING imagepath;`
 
-        var p string
+		var p string
 
-        results := h.DB.QueryRow(query, id)
-        err := results.Scan(&p)
-        if err != nil {
-            log.Println("Unable to Delete Photo",  err)
-        }
+		results := h.DB.QueryRow(query, id)
+		err := results.Scan(&p)
+		if err != nil {
+			log.Println("Unable to Delete Photo", err)
+		}
 
-        cmd := exec.Command("sudo", "rm", "-rf", fmt.Sprintf("*%v", p))
-        cmd.Dir = "/mnt/usb/images"
+		cmd := exec.Command("sudo", "rm", "-rf", fmt.Sprintf("*%v", p))
+		cmd.Dir = "/mnt/usb/images"
 
-        err = cmd.Run()
-        if err != nil {
-            log.Println("UNABLE TO DELETE ROW")
-            log.Println(err)
-        }
+		err = cmd.Run()
+		if err != nil {
+			log.Println("UNABLE TO DELETE ROW")
+			log.Println(err)
+		}
 
-    })
+	})
 }
 
 func (h AdminHandler) HandlerAdminDeleteShow() http.HandlerFunc {
@@ -127,93 +127,91 @@ func (h AdminHandler) HandlerAdminDeleteShow() http.HandlerFunc {
 }
 
 func ImageProcess(file multipart.File, header *multipart.FileHeader, i *types.Photo) error {
-		contentType := header.Header["Content-Type"][0]
-		log.Printf("Content-Type:: %v\n", contentType)
+	contentType := header.Header["Content-Type"][0]
+	log.Printf("Content-Type:: %v\n", contentType)
 
-		var osFile *os.File
-        var err error
+	var osFile *os.File
+	var err error
 
-		if contentType == "image/jpeg" {
-			osFile, err = os.CreateTemp("/mnt/usb/images/", "*.jpg")
+	if contentType == "image/jpeg" {
+		osFile, err = os.CreateTemp("/mnt/usb/images/", "*.jpg")
 
-		} else if contentType == "image/png" {
-			osFile, err = os.CreateTemp("/mnt/usb/images/", "*.png")
+	} else if contentType == "image/png" {
+		osFile, err = os.CreateTemp("/mnt/usb/images/", "*.png")
+	}
+	if err != nil {
+		return err
+	}
+	defer osFile.Close()
+
+	// SAVE FILE
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	osFile.Write(fileBytes)
+	defer osFile.Close()
+
+	// Get File Data
+	s, err := osFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	// Get File Date
+	year, month, day := ctime.Created(s).Local().Date()
+	i.Date = fmt.Sprintf("%v %v, %v", year, month, day)
+
+	//MAGICK EXECUTION
+	i.Image.FileName = filepath.Base(osFile.Name())
+	sizecmd := exec.Command("identify", "-format",
+		"'%[fx:w]x%[fx:h]'",
+		osFile.Name())
+
+	size, err := sizecmd.Output()
+	if err != nil {
+		return err
+	}
+	fmt.Println(size)
+	xy := strings.TrimFunc(string(size[:]), func(r rune) bool {
+		if r == '\'' || r == ' ' {
+			return true
 		}
-		if err != nil {
-            return err
-		}
-		defer osFile.Close()
+		return false
+	})
+	xy_str := strings.Split(xy, "x")
+	i.Image.Width = xy_str[0]
+	i.Image.Height = xy_str[1]
 
-		// SAVE FILE
-		fileBytes, err := io.ReadAll(file)
-		if err != nil {
-            return err
-		}
-		osFile.Write(fileBytes)
-		defer osFile.Close()
+	var cmds []*exec.Cmd
 
-        // Get File Data
-		s, err := osFile.Stat()
-		if err != nil {
-            return err
-		}
+	fmt.Println("---RUNNING MAGICK---")
+	mincmd := exec.Command("sudo", "magick",
+		osFile.Name(), "-resize", "50x50",
+		filepath.Dir(osFile.Name())+"/min_"+i.Image.FileName)
 
-        // Get File Date
-		year, month, day := ctime.Created(s).Local().Date()
-		i.Date = fmt.Sprintf("%v %v, %v", year, month, day)
-        
+	smcmd := exec.Command("sudo", "magick",
+		osFile.Name(), "-resize", "1000000@\\>",
+		filepath.Dir(osFile.Name())+"/sm_"+i.Image.FileName)
 
-        //MAGICK EXECUTION
-		i.Image.FileName = filepath.Base(osFile.Name())
-        sizecmd := exec.Command("identify", "-format", 
-            "'%[fx:w]x%[fx:h]'",
-            osFile.Name())
+	medcmd := exec.Command("sudo", "magick",
+		osFile.Name(), "-resize", "1500000@\\>",
+		filepath.Dir(osFile.Name())+"/med_"+i.Image.FileName)
 
-        size, err := sizecmd.Output()
-        if err != nil{
-            return err
-        }
-        fmt.Println(size)
-        xy := strings.TrimFunc(string(size[:]), func(r rune) bool {
-            if r == '\'' || r == ' ' {
-                return true
-            }
-            return false
-        })
-        xy_str := strings.Split(xy, "x")
-        i.Image.Width = xy_str[0]
-        i.Image.Height = xy_str[1]
+	cmds = append(cmds, mincmd, smcmd, medcmd)
+	go magickCommand(cmds)
 
-        var cmds []*exec.Cmd
+	defer osFile.Close()
 
-
-        fmt.Println("---RUNNING MAGICK---")
-		mincmd := exec.Command("sudo", "magick",
-			osFile.Name(), "-resize", "50x50",
-			filepath.Dir(osFile.Name()) + "/min_"+ i.Image.FileName)
-
-		smcmd := exec.Command("sudo", "magick",
-			osFile.Name(), "-resize", "1000000@\\>",
-			filepath.Dir(osFile.Name()) + "/sm_"+ i.Image.FileName)
-
-		medcmd := exec.Command("sudo", "magick",
-			osFile.Name(), "-resize", "1500000@\\>",
-			filepath.Dir(osFile.Name()) + "/med_"+ i.Image.FileName)
-
-        cmds = append(cmds, mincmd, smcmd, medcmd)
-        go magickCommand(cmds)
-
-        defer osFile.Close()
-
-        return nil 
+	return nil
 }
 
 func magickCommand(cmds []*exec.Cmd) {
-    for i := range cmds{
-        err := cmds[i].Run()
-        if err != nil {
-            fmt.Println(err)
-        }
+	for i := range cmds {
+		err := cmds[i].Run()
+		if err != nil {
+			fmt.Println(err)
+		}
 
-    }
+	}
 }
